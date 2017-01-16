2a60f9a1aabafef996e44a71f357efca7244726e
马强 <m2012qiang@163.com>
2017-01-16 18:36:31 +0800

    Update function_point.c

--- function_point.c
+++ function_point.c
@@ -19,7 +19,7 @@ int process(int x, int y, funP fun)
 
 int main()
 {
-    printf("3 and 5 max is:%d\n" process(3,5,max));
-    printf("3 and 5 min is:%d\n" process(3,5,min));
+    printf("3 and 5 max is:%d\n", process(3,5,max));
+    printf("3 and 5 min is:%d\n", process(3,5,min));
     return 0;
 }
